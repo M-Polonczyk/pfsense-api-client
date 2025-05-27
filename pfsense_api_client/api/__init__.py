764c8a9c9e31dd0d4c9a4e8c58e23c00d957158d
M-Polonczyk <m.polonczyk@outlook.com>
2025-05-27 23:50:25 +0200

    test

--- pfsense_api_client/api/__init__.py
+++ pfsense_api_client/api/__init__.py
@@ -1,15 +1,15 @@
 # flake8: noqa
 
 # import apis into api package
-from .auth_api import AUTHApi
-from .diagnostics_api import DIAGNOSTICSApi
-from .firewall_api import FIREWALLApi
-from .graphql_api import GRAPHQLApi
-from .interface_api import INTERFACEApi
-from .routing_api import ROUTINGApi
-from .services_api import SERVICESApi
-from .status_api import STATUSApi
-from .system_api import SYSTEMApi
-from .user_api import USERApi
-from .vpn_api import VPNApi
+from pfsense_api_client.api.auth_api import AUTHApi
+from pfsense_api_client.api.diagnostics_api import DIAGNOSTICSApi
+from pfsense_api_client.api.firewall_api import FIREWALLApi
+from pfsense_api_client.api.graphql_api import GRAPHQLApi
+from pfsense_api_client.api.interface_api import INTERFACEApi
+from pfsense_api_client.api.routing_api import ROUTINGApi
+from pfsense_api_client.api.services_api import SERVICESApi
+from pfsense_api_client.api.status_api import STATUSApi
+from pfsense_api_client.api.system_api import SYSTEMApi
+from pfsense_api_client.api.user_api import USERApi
+from pfsense_api_client.api.vpn_api import VPNApi
 
